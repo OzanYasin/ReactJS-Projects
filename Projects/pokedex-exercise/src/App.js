3327aa95caa4f3eb6a461b116726ae8cf2523df1
Ozan <ozan.yasin97@gmail.com>
2022-07-26 19:41:58 +0300

    pokedex-exercise added

---
+++ Projects/pokedex-exercise/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Pokegame />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Projects/pokedex-exercise/src/Pokedex.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Pokecard from './Pokecard';
+import './Pokedex.css';
+
+class Pokedex extends Component {
+  render() {
+    let title;
+    if (this.props.isWinner) {
+      title = <h1 className="Pokedex-winner">Winning Hand</h1>;
+    } else {
+      title = <h1 className="Pokedex-loser">Losing Hand</h1>;
+    }
+    return (
+      <div className="Pokedex">
+        <div>
+          {title}
+          <h4>Total Exp: {this.props.exp} </h4>
+        </div>
+        <div className="Pokedex-cards">
+          {this.props.pokemon.map((p) => (
+            <Pokecard
+              id={p.id}
+              name={p.name}
+              type={p.type}
+              exp={p.base_experience}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Pokedex;
