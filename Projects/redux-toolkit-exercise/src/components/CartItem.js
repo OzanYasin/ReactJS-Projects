6bfb5736cb8c337f48f5bcf372f6c2343b1a1966
Ozan <ozan.yasin97@gmail.com>
2022-07-27 15:58:28 +0300

    redux-toolkit-exercise updated

--- Projects/redux-toolkit-exercise/src/components/CartItem.js
+++ Projects/redux-toolkit-exercise/src/components/CartItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ChevronDown, ChevronUp } from '../icons';
-import { removeItem } from '../features/cart/cartSlice';
+import { removeItem, decrease, increase } from '../features/cart/cartSlice';
 import { useDispatch } from 'react-redux';
 
 const CartItem = ({ id, title, price, img, amount }) => {
@@ -21,11 +21,26 @@ const CartItem = ({ id, title, price, img, amount }) => {
         </button>
       </div>
       <div>
-        <button className="amount-btn">
+        <button
+          className="amount-btn"
+          onClick={() => {
+            dispatch(increase({ id }));
+          }}
+        >
           <ChevronUp />
         </button>
         <p className="amount">{amount}</p>
-        <button className="amount-btn">
+        <button
+          className="amount-btn"
+          onClick={() => {
+            if (amount === 1) {
+              dispatch(removeItem(id));
+              return;
+              // we do want to return due to not read the code below (dispatch(decrease({ id })))
+            }
+            dispatch(decrease({ id }));
+          }}
+        >
           <ChevronDown />
         </button>
       </div>
