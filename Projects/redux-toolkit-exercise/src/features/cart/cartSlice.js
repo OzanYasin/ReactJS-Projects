46beb1726d6d5cb92d2ce2c72a8fb8366ef8d44c
Ozan <ozan.yasin97@gmail.com>
2022-07-27 04:54:32 +0300

    redux-toolkit-exercise updated

--- Projects/redux-toolkit-exercise/src/features/cart/cartSlice.js
+++ Projects/redux-toolkit-exercise/src/features/cart/cartSlice.js
@@ -11,6 +11,18 @@ const initialState = {
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+      //return { cartItems: [] };
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+  },
 });
+
+export const { clearCart, removeItem } = cartSlice.actions;
 
 export default cartSlice.reducer;
