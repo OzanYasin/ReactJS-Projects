e6e386d56eb532b5d996782798b31bc06913c89a
Ozan <ozan.yasin97@gmail.com>
2022-09-04 19:06:38 +0300

    components updated and store folder created

--- Projects/meetup-exercise/src/pages/NewMeetup.js
+++ Projects/meetup-exercise/src/pages/NewMeetup.js
@@ -1,5 +1,28 @@
+import { useHistory } from 'react-router-dom';
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
 function NewMeetupPage() {
-  return <div>New Meetup Page</div>;
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch('https://meetup-exercise-default-rtdb.firebaseio.com/meetups.json', {
+      method: 'POST',
+      body: JSON.stringify(meetupData),
+      headers: {
+        // to make crystal clear that this request carries json data.
+        'Content-Type': 'application/json',
+      },
+    }).then(() => {
+      history.replace('/');
+    });
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
